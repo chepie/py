592296823f985be0f979924672237d09dac88c58
WINNIE CHEPTANUI CHERUIYOT <winniecheptanui001@gmail.com>
2024-06-09 10:58:45 +0300

    first code

---
+++ trial.py
@@ -0,0 +1,11 @@
+def age_years():
+    age =int(input('How old are you ?' )) 
+    if age >= 18:
+
+            print ('Old enough')
+     else :
+            print('Not yet there')
+    
+    
+
+age_years()
